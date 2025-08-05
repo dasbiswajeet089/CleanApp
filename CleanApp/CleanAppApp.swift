987b10444dc454323a5c8704178b67cde333fcb7
Biswajeet Das <dasbiswajeet021@gmail.com>
2025-08-06 01:17:08 +0800

    Initial Commit

---
+++ CleanApp/CleanAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanAppApp.swift
+//  CleanApp
+//
+//  Created by Biswajeet Das on 6/8/25.
+//
+
+import SwiftUI
+
+@main
+struct CleanAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
